66622bd3e6f774fd4ed9566afcc439083f0aba61
vmvivek291 <vivekabhayachar@gmail.com>
2025-03-18 01:25:46 +0530

    Updated hello.cpp with new changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "I have made changes";
+  cout << "I have made changes now";
   return 0;
 }
