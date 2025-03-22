75906649bfc7a993799ac69458b6ce7f430db2ba
vmvivek291 <142717908+vmvivek291@users.noreply.github.com>
2025-03-22 18:13:51 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "I have made changes now";
+  cout << "I am making changes for Task 3";
   return 0;
 }
